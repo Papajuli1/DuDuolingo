cba75f94acdc0507e3ab9013b7160ca497852105
Yonatan Abramovich <yonatan.abramovich@post.runi.ac.il>
2025-08-14 15:16:13 +0300

    Added Smartlook support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
+import smartlookClient from 'smartlook-client';
+
+smartlookClient.init('694fe74129901041c3dee54c4e8a93dc73dcfe33', { region: 'eu' });
+
+// Identify user in Smartlook if username exists in localStorage
+const smartlookUsername = localStorage.getItem('username');
+if (smartlookUsername) {
+  smartlookClient.identify(smartlookUsername);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
